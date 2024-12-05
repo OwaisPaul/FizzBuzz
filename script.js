a4baf17cfb0a01cbe4c4b6e6429c8db37d03a30e
OwaisPaul <152506988+OwaisPaul@users.noreply.github.com>
2024-12-05 19:35:42 +0530

    last part

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let answer = parseInt(prompt("Enter the number you want to FizzBuzz"));
 for(let i=1; i<= answer; i++){
-
+if(i % 3 ===0 & i % 5 ===0){
+    console.log("FizzBuzz");
+} else if(i % 3 ===0){
+    console.log("Fizz")
+}else if(i % 5 === 0){
+    console.log("Buzz")
+} 
 }
